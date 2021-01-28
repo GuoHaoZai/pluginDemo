af7dde020a6812581f26844706496c9208803c19
guohao <guohao@weli.cn>
2021-01-28 14:15:20 +0800

    Enhancement:添加在所有field上添加字符串的功能

---
+++ generator/src/main/java/guohao/generator/actions/GenerateFieldGeneralAction.java
@@ -0,0 +1,73 @@
+/*
+ *  Copyright (c) 2017-2019, bruce.ge.
+ *    This program is free software; you can redistribute it and/or
+ *    modify it under the terms of the GNU General Public License
+ *    as published by the Free Software Foundation; version 2 of
+ *    the License.
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *    GNU General Public License for more details.
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program;
+ */
+
+package guohao.generator.actions;
+
+import com.intellij.codeInsight.intention.PsiElementBaseIntentionAction;
+import com.intellij.codeInspection.util.IntentionFamilyName;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiField;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.IncorrectOperationException;
+import guohao.common.PsiDocumentUtils;
+import guohao.common.PsiToolUtils;
+import guohao.generator.BundleManager;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+/**
+ * @author guohao
+ * @since 2021/1/20
+ */
+public class GenerateFieldGeneralAction extends PsiElementBaseIntentionAction {
+
+    @Override
+    public void invoke(@NotNull Project project, Editor editor, @NotNull PsiElement element) throws IncorrectOperationException {
+        String fieldString = Messages.showInputDialog("", "输入插入的字符串", Messages.getInformationIcon());
+        PsiClass psiClass = PsiTreeUtil.getParentOfType(element, PsiClass.class);
+        PsiField[] psiFields = psiClass.getFields();
+
+        Document document = PsiDocumentUtils.getDocument(psiClass);
+        for (PsiField psiField : psiFields) {
+            TextRange textRange = psiField.getTextRange();
+            String formatString = PsiToolUtils.calculateFormatString(psiField);
+            document.insertString(textRange.getStartOffset(), fieldString + formatString);
+            PsiDocumentUtils.commitAndSaveDocument(psiClass, document);
+        }
+    }
+
+    @Override
+    public boolean isAvailable(@NotNull Project project, Editor editor, @NotNull PsiElement element) {
+        PsiClass parentOfType = PsiTreeUtil.getParentOfType(element, PsiClass.class);
+        return Objects.nonNull(parentOfType);
+    }
+
+    @NotNull
+    @Override
+    public String getText() {
+        return BundleManager.getGeneratorBundle("generator.field.general");
+    }
+
+    @Override
+    public @NotNull @IntentionFamilyName String getFamilyName() {
+        return BundleManager.getFamilyName("plugin.generator.family.name");
+    }
+}
